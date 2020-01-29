28192b07d7ae1456c562b7da6b3a61c5a3465e18
priyya0208 <priyya.0208@gmail.com>
2020-01-29 10:50:30 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = new FormControl('', Validators.required);
 }
